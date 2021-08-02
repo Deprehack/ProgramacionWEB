0e37d58bcdf47a354d48fcf47caf452acd73212c
Deprehack <73446015+Deprehack@users.noreply.github.com>
2021-08-01 22:36:06 -0500

    Add files via upload

---
+++ jsf-login/src/main/java/mx/com/mentoringit/beans/UserBean.java
@@ -0,0 +1,38 @@
+package mx.com.mentoringit.beans;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name="user")
+@SessionScoped
+public class UserBean implements Serializable{
+	
+	private String name = "";
+	private String password;
+	
+	public String login() {
+		String result="";
+		if(name.length() > 0 && password.length() > 0) {
+			result = "weelcome";
+		}else {
+			result = "index";
+		}
+		return result;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
